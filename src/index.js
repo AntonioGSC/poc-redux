0e03c451e17a2cf29b1d9a20aa2376fc89ca4014
AntonioGSC <antoniogabrielagsc@gmail.com>
2023-03-29 22:21:41 -0300

    init redux

--- src/index.js
+++ src/index.js
@@ -2,16 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore } from 'redux';
+
+// //ACTION INCREMENT
+// const increment = () => {
+//     return {
+//         type: 'INCREMENT'
+//     }
+// }
+
+// const decrement = () => {
+//     return {
+//         type: 'DECREMENT'
+//     }
+// }
+
+// //REDUCER
+// const counter = (state = 0, action) => {
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return state + 1;
+//         case 'DECREMENT':
+//             return state - 1;
+//         default:
+//             break;
+//     }
+// }
+
+// //STORE -> GLOBALIZED STATE
+// let store = createStore(counter);
+
+// //Display it in the console
+// store.subscribe(() => console.log(store.getState()));
+
+// //DISPATCH
+// store.dispatch(increment());
+// store.dispatch(increment());
+// store.dispatch(decrement());
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
